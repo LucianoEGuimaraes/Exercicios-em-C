2101092f6a0f7d39301a83d9a909faa475eb3145
Luciano Guimarães <88801763+LucianoEGuimaraes@users.noreply.github.com>
2022-05-07 01:16:20 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+    int chute;
+    int contador = 2;
+
+    //------------- Biblioteca de sorteios -----
+    srand(time(NULL));
+    int teste = rand() % 11;
+    //------------------
+
+    //------ liberar essa variavel para ver o sorteio
+    //printf("%d", teste);
+    //------------------
+
+    while (contador >=0) {
+    printf("Tente adivinhar qual o número, dê um chute: \n");
+    scanf("%d", &chute);
+
+        if (chute > teste){
+            printf ("O número está abaixo do seu chute.\n");
+            printf ("Tentativas restantes %d \n\n", contador);
+            contador --;
+        }else if (chute < teste){
+            printf ("O número está acima do seu chute.\n");
+            printf ("Tentativas restantes %d \n\n", contador);
+            contador --;
+        }else{
+            printf ("Parabéns, você acertou! \n\n");
+            contador = 0;
+        }
+    }
+    return 0;
+}
